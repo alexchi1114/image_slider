4db150b392d9abe93044730405c65c647fcb7f2b
AlexChi <alexchi1114@gmail.com>
2017-06-28 18:44:41 -0400

    finished functionality

--- scripts/main.js
+++ scripts/main.js
@@ -8,9 +8,9 @@ var nextImage = 1;
 var slideNextImageLeft = function(){
 	$('#image-'+currentImage).animate({left:-700+(-700*(currentImage))},1000);
 	$('#image-'+nextImage).animate({left:-700+(-700*(nextImage-1))},1000);
-	previousImage = currentImage;
 	currentImage = nextImage;
 	nextImage = ((nextImage)+1)%5;
+	previousImage = (nextImage+3)%5;
 	$('#image-'+nextImage).css({left:-700+(-700*(nextImage-2))});
 };
 
@@ -23,12 +23,58 @@ var slidePreviousImageRight = function(){
 	previousImage = (currentImage+4)%5;
 };
 
+var createNavigation = function(){
+	
+	for(i=0;i<5;i++){
+		$('#bubbles').append(`<div class = nav-bubble id = nav-${i}></div>`);
+	}
+}
+
+var selectLocation = function(element){
+	var id = element.attr('id');
+	var number = parseInt(id[id.length-1]);
+	nextImage = number;
+	slideNextImageLeft();
+	console.log(previousImage);
+	console.log(currentImage);
+	console.log(nextImage);
+	
+}
+
+var fillInBubbles = function(){
+	$('.nav-bubble').css('opacity','0.3');
+	$('#nav-'+currentImage).css('opacity','0.9');
+}
+
 $(document).ready(function(){
+
+
+	createNavigation();
+
 	$('#next-button').click(function(){
 		slideNextImageLeft();
+		fillInBubbles();
 	});	
 
 	$('#previous-button').click(function(){
 		slidePreviousImageRight();
+		fillInBubbles();
+	});
+
+	$('.nav-bubble').click(function(){
+		selectLocation($(this));
+		fillInBubbles();
 	});
 });
+
+0 - 3
+
+1 - 4
+
+2 - 0
+
+3 -1
+
+4 - 2
+
+
